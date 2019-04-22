a1034523eda269d55c8d7b3a6ad24efdc57e31b1
Felipe Cook <felipepcook@gmail.com>
2019-04-22 14:48:51 -0600

    Solved PlayingCat

--- src/edu/cnm/deepdive1/PlayingCat.java
+++ src/edu/cnm/deepdive1/PlayingCat.java
@@ -2,4 +2,15 @@ package edu.cnm.deepdive1;
 
 public class PlayingCat {
 
+  public static boolean isCatPlaying(boolean summer, int temperature) {
+
+    if (summer && (temperature > 24 && temperature < 46)) {
+      return true;
+    } else if (!summer && (temperature > 24 && temperature < 36)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
