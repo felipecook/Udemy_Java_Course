238ca32537c5f82b2a0697d7a275e7907d7b9dfc
Felipe Cook <felipepcook@gmail.com>
2019-05-01 23:15:56 -0600

    Solving NumberToWords Udemy

---
+++ src/edu/cnm/deepdive1/NumberToWords.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive1;
+
+public class NumberToWords {
+
+  public static void wordsToDigits (int number) {
+    if (number < 0) {
+      System.out.println("Invalid Value");
+    }
+
+  }
+
+  public static int reverse (int number) {
+
+    
+  }
+
+
+}
