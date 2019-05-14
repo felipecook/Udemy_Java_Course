67697fd664d8ea41f90c8d27bc7ba7b5eb8f1cf5
Felipe Cook <felipepcook@gmail.com>
2019-05-13 20:10:05 -0700

    Solved Equality Printer

--- src/edu/cnm/deepdive1/IntEqualityPicker.java
+++ src/edu/cnm/deepdive1/IntEqualityPicker.java
@@ -2,4 +2,17 @@ package edu.cnm.deepdive1;
 
 public class IntEqualityPicker {
 
+  public static void printEqual(int varA, int varB, int varC) {
+    if (varA <= 0 || varB <= 0 || varC <= 0) {
+      System.out.println("Invalid Value");
+    } else if (varA == varC && varA == varB) {
+      System.out.println("All numbers are equal");
+    } else if (varA != varC && varB != varC && varA != varB) {
+      System.out.println("All numbers are different");
+    } else {
+      System.out.println("Neither all are equal or different");
+    }
+  }
+  
+
 }
